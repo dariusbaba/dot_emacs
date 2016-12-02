5ae2ea4e837f75aa5d4a306fceae2bc61f445c74
Darius Baba <baba.darius@gmail.com>
2016-12-02 15:27:15 +0200

    Local eslint config is now used rather than the global one

--- bundles/anything-bundle.el
+++ bundles/anything-bundle.el
@@ -323,7 +323,8 @@ Repeated invocations toggle between the two most recently open buffers."
 ;; (load-theme 'tango-dark)
 ;; (load-theme 'junio)
 
-(load-theme 'molokai)
+;; (load-theme 'molokai)
+(load-theme 'tsdh-dark)
 
 (require 'pbcopy)
 (turn-on-pbcopy)
@@ -356,6 +357,18 @@ Repeated invocations toggle between the two most recently open buffers."
 (add-to-list 'auto-mode-alist '("\\.js?\\'" . web-mode))
 
 ;; disable jshint since we prefer eslint checking
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
 (setq-default flycheck-disabled-checkers
   (append flycheck-disabled-checkers '(javascript-jshint)))
 
